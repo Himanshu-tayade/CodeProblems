c27ec0e73f63b107007200c53384828216ced473
Himanshu-tayade <himanshutayade031@gmail.com>
2024-07-27 00:00:58 +0530

    BS Leetcode 2064

---
+++ MinMax_Stores.java
@@ -0,0 +1,32 @@
+class Solution {
+    public boolean isPossible(int[] arr,int maxQ, int n) {
+        int stores = 0;
+        for(int i = 0 ; i < arr.length ; i++) {
+            if(arr[i] % maxQ == 0) stores += arr[i]/maxQ;
+            else stores += arr[i]/maxQ + 1;
+        }
+        if(stores > n) return false;
+        else return true;
+    }
+    public int minimizedMaximum(int n, int[] arr) {
+        int l = arr.length;
+        int mx = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0 ; i < l ; i ++) {
+            mx = Math.max(mx, arr[i]);
+            sum += arr[i];
+        }
+        int lo = 1 , hi = mx , ans = mx;
+        while(lo <= hi) {
+            int mid = lo + (hi - lo)/2;
+            if(isPossible(arr,mid,n)) {
+                ans = mid;
+                hi = mid - 1;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
